4700abe5921718e2e585b15c307d4edc02a118c2
Alex.k <Alex.k@ramotion.com>
2015-12-30 15:08:31 +0300

    added travis file

---
+++ FoldingCell/FoldingCellFramework/FoldingCellFramework.h
@@ -0,0 +1,19 @@
+//
+//  FoldingCellFramework.h
+//  FoldingCellFramework
+//
+//  Created by Alex K. on 30/12/15.
+//  Copyright © 2015 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoldingCellFramework.
+FOUNDATION_EXPORT double FoldingCellFrameworkVersionNumber;
+
+//! Project version string for FoldingCellFramework.
+FOUNDATION_EXPORT const unsigned char FoldingCellFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoldingCellFramework/PublicHeader.h>
+
+
