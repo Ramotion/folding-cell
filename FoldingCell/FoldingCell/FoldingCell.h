739c320ae9ab2685212e724d50543d16264d81a8
Alex.k <Alex.k@ramotion.com>
2016-06-02 14:50:19 +0300

    shared scheme

---
+++ FoldingCell/FoldingCell/FoldingCell.h
@@ -0,0 +1,19 @@
+//
+//  FoldingCell.h
+//  FoldingCell
+//
+//  Created by Alex K. on 02/06/16.
+//  Copyright © 2016 Alex K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoldingCell.
+FOUNDATION_EXPORT double FoldingCellVersionNumber;
+
+//! Project version string for FoldingCell.
+FOUNDATION_EXPORT const unsigned char FoldingCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoldingCell/PublicHeader.h>
+
+
