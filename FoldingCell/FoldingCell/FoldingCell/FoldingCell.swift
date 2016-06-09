4400c84731a8df9c8874263bea6979da634f2179
Alex.k <Alex.k@ramotion.com>
2016-06-09 14:30:52 +0300

    small fix

--- FoldingCell/FoldingCell/FoldingCell/FoldingCell.swift
+++ FoldingCell/FoldingCell/FoldingCell/FoldingCell.swift
@@ -54,6 +54,10 @@ public class FoldingCell: UITableViewCell {
   
   // MARK:  life cicle
   
+  override public init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+  
   required public init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
   }
