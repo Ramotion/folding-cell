73da5d0c1414f56cc17e35cf31e3f78b898f6920
Abdurahim Jauzee <jauzee@ramotion.com>
2017-05-29 19:04:31 +0300

    separate library source files from demo app module

--- FoldingCell/FoldingCell-Demo/DemoCell.swift
+++ FoldingCell/FoldingCell-Demo/DemoCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FoldingCell
 
 class DemoCell: FoldingCell {
   
@@ -19,25 +20,25 @@ class DemoCell: FoldingCell {
       openNumberLabel.text = String(number)
     }
   }
+  
   override func awakeFromNib() {
-    
     foregroundView.layer.cornerRadius = 10
     foregroundView.layer.masksToBounds = true
-    
     super.awakeFromNib()
   }
   
   override func animationDuration(_ itemIndex:NSInteger, type:AnimationType)-> TimeInterval {
-    
     let durations = [0.26, 0.2, 0.2]
     return durations[itemIndex]
   }
+  
 }
 
-// MARK: Actions
+// MARK: - Actions ⚡️
 extension DemoCell {
   
   @IBAction func buttonHandler(_ sender: AnyObject) {
     print("tap")
   }
+  
 }
---
+++ FoldingCell/FoldingCell-Demo/MainTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  MainTableViewController.swift
+//
+// Copyright (c) 21/12/15. Ramotion Inc. (http://ramotion.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    
+    let kCloseCellHeight: CGFloat = 179
+    let kOpenCellHeight: CGFloat = 488
+
+    let kRowsCount = 10
+    
+    var cellHeights = [CGFloat]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = kCloseCellHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        createCellHeightsArray()
+        self.tableView.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+    }
+    
+    // MARK: configure
+    func createCellHeightsArray() {
+        for _ in 0...kRowsCount {
+            cellHeights.append(kCloseCellHeight)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+      
+      guard case let cell as DemoCell = cell else {
+        return
+      }
+      
+      cell.backgroundColor = UIColor.clear
+      
+      if cellHeights[(indexPath as NSIndexPath).row] == kCloseCellHeight {
+        cell.selectedAnimation(false, animated: false, completion:nil)
+      } else {
+        cell.selectedAnimation(true, animated: false, completion: nil)
+      }
+      
+      cell.number = indexPath.row
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoldingCell", for: indexPath)
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeights[(indexPath as NSIndexPath).row]
+    }
+    
+    // MARK: Table vie delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
+        
+        if cell.isAnimating() {
+            return
+        }
+        
+        var duration = 0.0
+        if cellHeights[(indexPath as NSIndexPath).row] == kCloseCellHeight { // open cell
+            cellHeights[(indexPath as NSIndexPath).row] = kOpenCellHeight
+            cell.selectedAnimation(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {// close cell
+            cellHeights[(indexPath as NSIndexPath).row] = kCloseCellHeight
+            cell.selectedAnimation(false, animated: true, completion: nil)
+            duration = 0.8
+        }
+        
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }, completion: nil)
+
+        
+    }
+}
