0799d85177775757747e86fa3ee5d52e371873d0
Abdurahim Jauzee <jauzee@ramotion.com>
2017-05-30 15:43:06 +0300

    minor improvements

--- FoldingCell/FoldingCell-Demo/MainTableViewController.swift
+++ FoldingCell/FoldingCell-Demo/MainTableViewController.swift
@@ -22,90 +22,86 @@
 // THE SOFTWARE.
 
 import UIKit
+import FoldingCell
 
 class MainTableViewController: UITableViewController {
-    
-    let kCloseCellHeight: CGFloat = 179
-    let kOpenCellHeight: CGFloat = 488
+  
+  let kCloseCellHeight: CGFloat = 179
+  let kOpenCellHeight: CGFloat = 488
+  let kRowsCount = 10
+  var cellHeights: [CGFloat] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+  }
+  
+  private func setup() {
+    cellHeights = Array(repeating: kCloseCellHeight, count: kRowsCount)
+    tableView.estimatedRowHeight = kCloseCellHeight
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "background"))
+  }
+  
+}
 
-    let kRowsCount = 10
-    
-    var cellHeights = [CGFloat]()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.estimatedRowHeight = kCloseCellHeight
-        tableView.rowHeight = UITableViewAutomaticDimension
-        createCellHeightsArray()
-        self.tableView.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
+// MARK: - TableView
+extension MainTableViewController {
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    guard case let cell as DemoCell = cell else {
+      return
     }
     
-    // MARK: configure
-    func createCellHeightsArray() {
-        for _ in 0...kRowsCount {
-            cellHeights.append(kCloseCellHeight)
-        }
-    }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-      
-      guard case let cell as DemoCell = cell else {
-        return
-      }
-      
-      cell.backgroundColor = UIColor.clear
-      
-      if cellHeights[(indexPath as NSIndexPath).row] == kCloseCellHeight {
-        cell.selectedAnimation(false, animated: false, completion:nil)
-      } else {
-        cell.selectedAnimation(true, animated: false, completion: nil)
-      }
-      
-      cell.number = indexPath.row
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FoldingCell", for: indexPath)
-
-        return cell
+    cell.backgroundColor = .clear
+    
+    if cellHeights[indexPath.row] == kCloseCellHeight {
+      cell.selectedAnimation(false, animated: false, completion:nil)
+    } else {
+      cell.selectedAnimation(true, animated: false, completion: nil)
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return cellHeights[(indexPath as NSIndexPath).row]
+    cell.number = indexPath.row
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FoldingCell", for: indexPath)
+    return cell
+  }
+  
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return cellHeights[indexPath.row]
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
+    
+    if cell.isAnimating() {
+      return
     }
     
-    // MARK: Table vie delegate
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
-        
-        if cell.isAnimating() {
-            return
-        }
-        
-        var duration = 0.0
-        if cellHeights[(indexPath as NSIndexPath).row] == kCloseCellHeight { // open cell
-            cellHeights[(indexPath as NSIndexPath).row] = kOpenCellHeight
-            cell.selectedAnimation(true, animated: true, completion: nil)
-            duration = 0.5
-        } else {// close cell
-            cellHeights[(indexPath as NSIndexPath).row] = kCloseCellHeight
-            cell.selectedAnimation(false, animated: true, completion: nil)
-            duration = 0.8
-        }
-        
-        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
-            tableView.beginUpdates()
-            tableView.endUpdates()
-        }, completion: nil)
-
-        
+    var duration = 0.0
+    let cellIsCollapsed = cellHeights[indexPath.row] == kCloseCellHeight
+    if cellIsCollapsed {
+      cellHeights[indexPath.row] = kOpenCellHeight
+      cell.selectedAnimation(true, animated: true, completion: nil)
+      duration = 0.5
+    } else {
+      cellHeights[indexPath.row] = kCloseCellHeight
+      cell.selectedAnimation(false, animated: true, completion: nil)
+      duration = 0.8
     }
+    
+    UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+      tableView.beginUpdates()
+      tableView.endUpdates()
+    }, completion: nil)
+    
+  }
+  
 }
