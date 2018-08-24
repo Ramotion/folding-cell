a102990f70ad862b222702acee4cd046d42e4a95
Alex K <kalinkin.aleksey@gmail.com>
2018-08-24 10:58:25 +0300

    add Tests target

---
+++ FoldingCell/FoldingCellTests/FoldingCellTests.swift
@@ -0,0 +1,20 @@
+//
+//  FoldingCellTests.swift
+//  FoldingCellTests
+//
+//  Created by Alex K on 24/08/2018.
+//  Copyright © 2018 Alex K. All rights reserved.
+//
+
+import XCTest
+
+class FoldingCellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
